014e266e52513faa8922510de139905856cc03e8
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2020-04-30 08:31:52 +0200

    Added new plant model, updated existing plant model, added new worlds

--- models/install_models.sh
+++ models/install_models.sh
@@ -1,2 +1,2 @@
 mkdir -p $HOME/.gazebo/models
-cp -a capsicum_plant_6 Floor_room grey_wall $HOME/.gazebo/models
+cp -a capsicum_plant_6 capsicum_plant_6_no_fruits Floor_room grey_wall $HOME/.gazebo/models
