33dd81facdce61252ab446caee4784c63ab74168
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2021-02-17 14:29:19 +0100

    Added model with one fruit, move to see test world

--- models/install_models.sh
+++ models/install_models.sh
@@ -1,2 +1,2 @@
 mkdir -p $HOME/.gazebo/models
-cp -a capsicum_plant_6 capsicum_plant_6_no_fruits Floor_room grey_wall $HOME/.gazebo/models
+cp -a capsicum_plant_6 capsicum_plant_6_no_fruits capsicum_plant_6_one_fruit Floor_room grey_wall $HOME/.gazebo/models
