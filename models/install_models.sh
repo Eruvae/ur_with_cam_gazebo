d00c6e93757e299f891b28ebf4e9271b1f251730
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2022-03-23 10:55:37 +0100

    Add new plant models

--- models/install_models.sh
+++ models/install_models.sh
@@ -1,2 +1,2 @@
 mkdir -p $HOME/.gazebo/models
-cp -a capsicum_plant_6 capsicum_plant_6_no_fruits capsicum_plant_6_one_fruit capsicum_plant_6_more_occ Floor_room grey_wall $HOME/.gazebo/models
+cp -a $(ls -d */) $HOME/.gazebo/models
